125a50e5c234da88c5114318c24cb91f6e894546
Alexander Potashev <aspotashev@gmail.com>
2010-10-28 05:12:07 +0400

    cleanup

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -9,18 +9,13 @@ class AdminController < ApplicationController
   end
   
   
-#  verify :method => :post, :xhr => true, :only => :ajax_lock_pofile,
-#      :redirect_to => :home_url, :render => "hello"
+  verify :method => :post, :xhr => true, :only => :ajax_lock_pofile,
+      :redirect_to => :home_url, :render => "hello"
   def ajax_lock_pofile
-    p TranslationFile.find(params[:id]).user_locked
-    p '-------'
     x = TranslationFile.find(params[:id])
-    p x
     x.unlock
     x.user_locked = -1
-    p x
-    p '-------'
-    p TranslationFile.find(params[:id]).user_locked
+
     respond_to do |format|
       format.xml {
 	render :partial => 'div_po_locking', :locals => { :file => TranslationFile.find(params[:id].to_i) }
