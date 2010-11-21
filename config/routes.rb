ecd5c1d7ef533b6690c17039436bc7e088959ccc
Alexander Potashev <aspotashev@gmail.com>
2010-11-21 17:40:50 +0300

    open '<DUMP>' translation file when browsing '/'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
-  map.home '', :controller => 'admin', :action => 'test_jquery'
+#  map.home '', :controller => 'admin', :action => 'test_jquery'
+  map.home '', :controller => 'translation_file', :action => 'index', :id => 1
 
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
