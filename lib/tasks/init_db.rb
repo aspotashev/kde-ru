7381ae534280ef73073e613a0d8148a2d2652be8
Alexander Potashev <aspotashev@gmail.com>
2010-11-23 02:35:14 +0300

    Add 'init_db' rake task

---
+++ lib/tasks/init_db.rb
@@ -0,0 +1,16 @@
+
+namespace :db do
+  desc "Erase and fill database"
+
+  task :init_db => :environment do
+    FileContent.delete_all
+    OpenIdAuthenticationAssociation.delete_all
+    OpenIdAuthenticationNonce.delete_all
+    TranslationFile.delete_all
+    User.delete_all
+
+
+    TranslationFile.create(:filename_with_path => '<DUMP>')
+  end
+end
+
