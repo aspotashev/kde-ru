c2a5b0133cb1d550354f1d28df16cc44c86f98ec
Alexander Potashev <aspotashev@gmail.com>
2010-12-12 23:24:13 +0300

    disable debugging output

--- vendor/plugins/pr_isearch/lib/isearch.rb
+++ vendor/plugins/pr_isearch/lib/isearch.rb
@@ -7,9 +7,9 @@ IndexSearch.init(conf['dump'], conf['dump_index'], conf['dump_map'])
 
 IndexSearch.class_eval do
   def self.request(s,n)
-    p s
-    p n
-    p find(s,n)
+#    p s
+#    p n
+#    p find(s,n)
 
     find(s,n).map do |id|
       r = id.split(':')
