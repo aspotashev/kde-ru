ac1447f49501a2176287b87482b7012c4f8fbff3
Alexander Potashev <aspotashev@gmail.com>
2010-12-14 22:24:23 +0300

    allow watching the main page without logging in

--- app/controllers/translation_file_controller.rb
+++ app/controllers/translation_file_controller.rb
@@ -1,5 +1,5 @@
 class TranslationFileController < ApplicationController
-  before_filter :login_required
+#  before_filter :login_required
 
   def index
     @file = TranslationFile.find(params[:id])
