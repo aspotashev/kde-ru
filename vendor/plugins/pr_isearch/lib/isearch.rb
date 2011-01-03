907cd4f9df1096ccbce6f02e21e24c8a1a7e3e05
Alexander Potashev <aspotashev@gmail.com>
2011-01-03 05:57:48 +0300

    use isearch through po-backend
    This removes huge isearch database from Rails app's memory.

--- vendor/plugins/pr_isearch/lib/isearch.rb
+++ vendor/plugins/pr_isearch/lib/isearch.rb
@@ -1,24 +1,18 @@
 
-conf = YAML::load(File.open(File.dirname(__FILE__) + '/../config.yml'))
-$isearch_conf_yaml = conf # workaround to pass 'conf' into class IndexSearch
-
-require conf['binary_ruby_module']
-IndexSearch.init(conf['dump'], conf['dump_index'], conf['dump_map'])
-
-IndexSearch.class_eval do
+class IndexSearch
   def self.request(s,n)
 #    p s
 #    p n
 #    p find(s,n)
 
-    find(s,n).map do |id|
+    $po_backend.isearch.find(s,n).map do |id|
       r = id.split(':')
       [r[0], r[1].to_i]
     end
   end
 
   def self.db_config
-    YAML::load(File.open($isearch_conf_yaml['db_config']))
+    YAML::load(File.open('/home/sasha/python-l10n-db-server/generate-dump/database.yml'))
   end
 end
 
