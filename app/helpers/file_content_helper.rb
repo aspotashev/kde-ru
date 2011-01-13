7c592a2c89d010271a62814f271285e2eab078e6
Alexander Potashev <aspotashev@gmail.com>
2011-01-13 18:44:13 +0300

    cleanup

--- app/helpers/file_content_helper.rb
+++ app/helpers/file_content_helper.rb
@@ -3,21 +3,6 @@ module FileContentHelper
 
   def posieve_check_rules_fill_cache(file_content)
     Stalker.enqueue("pology_check", :file_content_id => file_content.id)
-    return
-
-
-    $po_backend.error_hook = lambda {|s| flash[:error] = s }
-    if posieve = $po_backend.posieve
-      res = posieve.check_rules(file_content.content.to_file.read)
-
-      if res
-        file_content.pology_errors_cache = res.to_yaml
-        file_content.pology_errors_count_cache = res.size
-        file_content.save!
-      end
-    else
-      nil
-    end
   end
 
   # file_content is an instance of FileContent model class
@@ -41,8 +26,6 @@ module FileContentHelper
       posieve_check_rules_fill_cache(file_content)
     end
 
-    puts "file_content.pology_errors_count_cache:"
-    p file_content.pology_errors_count_cache
     file_content.pology_errors_count_cache
   end
 end
