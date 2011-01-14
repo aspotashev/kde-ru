48463919e73bf31641e478108832ec5318aa7e13
Alexander Potashev <aspotashev@gmail.com>
2011-01-14 17:34:22 +0300

    configure god to run po-backend

--- config/god.rb
+++ config/god.rb
@@ -1,13 +1,7 @@
-# see Railscasts 243: beanstalkd and stalker
+# see Railscasts 243: beanstalkd and stalker; Railscasts 130: monitoring with god
 RAILS_ROOT = File.expand_path("../..", __FILE__)
 
-God.watch do |w|
-  w.name = "stalk-general-jobs"
-  w.interval = 30.seconds
-  w.env = {"RAILS_ENV" => "production"} # does it work with Rails 2.3.10?
-  w.start = "/usr/bin/stalk #{RAILS_ROOT}/app/jobs/jobs.rb"
-  w.log = "#{RAILS_ROOT}/log/stalker.log"
-
+def generic_monitoring(w, options = {})
   w.start_if do |start|
     start.condition(:process_running) do |c|
       c.running = false
@@ -16,7 +10,8 @@ God.watch do |w|
 
   w.restart_if do |restart|
     restart.condition(:memory_usage) do |c|
-      c.above = 70.megabytes
+      c.above = options[:memory_limit]
+      c.times = 5
     end
   end
 
@@ -31,4 +26,23 @@ God.watch do |w|
       c.retry_within = 2.hours
     end
   end
+end
+
+God.watch do |w|
+  w.name = "stalk-general-jobs"
+  w.interval = 30.seconds
+  w.env = {"RAILS_ENV" => "production"} # does it work with Rails 2.3.10?
+  w.start = "/usr/bin/stalk #{RAILS_ROOT}/app/jobs/jobs.rb"
+  w.log = "#{RAILS_ROOT}/log/stalker.log"
+
+  generic_monitoring(w, :memory_limit => 70.megabytes)
+end
+
+God.watch do |w|
+  w.name = "po-backend-drb"
+  w.interval = 30.seconds
+  w.start = "#{RAILS_ROOT}/vendor/po-backend/server.rb"
+  w.log = "#{RAILS_ROOT}/log/po-backend.log"
+
+  generic_monitoring(w, :memory_limit => 40.megabytes) # usually it takes 30 MB
 end
