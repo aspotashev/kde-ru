3a3b58b02df2e88fa3657f6df69ecbaa3e072e9e
Alexander Potashev <aspotashev@gmail.com>
2011-05-02 09:51:33 +0400

    Remove posearch

--- app/controllers/posearch_controller.rb
+++ app/controllers/posearch_controller.rb
@@ -1,28 +1,4 @@
 class PosearchController < ApplicationController
   def index
   end
-
-  def auto_complete_request
-    search = params['text'].to_s
-
-    if search.empty?
-      render :partial => 'search_results'
-      return
-    end
-
-    message_ids = IndexSearch.request(search, 20) # pairs (filename, index)
-#    message_ids = message_ids.map {|id| [id[0], id[1]] }.flatten
-
-    @msgs = message_ids.map do |id|
-      PoMessageEntry.find_by_filename_and_index(id[0], id[1])
-    end
-
-    @msgs.select! {|x| not x[:msgid].empty? } # HACK
-
-    # These do not work:
-    #@msgs = PoMessageEntry.find_by_filename_and_index(message_ids)
-    #@msgs = PoMessageEntry.find_by_filename_and_index(['messages/kdeutils/ark.po', 'messages/kdeutils/filelight.po'], [8, 28])
-
-    render :partial => 'search_results'
-  end
 end
--- app/models/po_message_entry.rb
+++
@@ -1,8 +0,0 @@
-class PoMessageEntry < ActiveRecord::Base
-  # TODO: move this to the plugin, just call 'include' here
-
-  set_table_name :po_messages
-
-  establish_connection(IndexSearch.db_config)
-end
-
