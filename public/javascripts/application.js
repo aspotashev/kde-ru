9327c3e69b68163486032ecad7011bbea0a86f50
Alexander Potashev <aspotashev@gmail.com>
2011-07-16 21:53:03 +0400

    panel on the left side

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function(){
+
+//      $("table").delegate("a", "click", function(){
+  $("a.ajax_lock").live("click", function(){ // people say that "live" fails on some browsers
+    clicked_link_id = $(this).attr('id');
+    $.post('ajax_lock_pofile/' + clicked_link_id.substr(1), function(data){
+      $("tr.ajax_lock_div#" + clicked_link_id).html(data);
+    }, {}, {}, 'html');
+  });
+
+});
