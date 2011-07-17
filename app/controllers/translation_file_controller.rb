253b9fd5600148d1c5e4bc36f792dd4e6911038e
Alexander Potashev <aspotashev@gmail.com>
2011-07-17 14:03:27 +0400

    switch directories

--- app/controllers/translation_file_controller.rb
+++ app/controllers/translation_file_controller.rb
@@ -4,9 +4,23 @@ class TranslationFileController < ApplicationController
   def index
     @file = TranslationFile.find(params[:id])
   end
-  
+
   def polist
-    @files = TranslationFile.find(:all) # TODO: filter only files with moved_to=-1
+    @category_list = {
+      'extragear-games' => 'sidebar-item-1',
+      'extragear-multimedia' => 'sidebar-item-1',
+      'kdebase' => 'sidebar-item-2',
+      'kdelibs' => 'sidebar-item-2',
+      'playground-pim' => 'sidebar-item-3',
+    }
+
+    @selected_cat = params[:id] if @category_list.has_key?(params[:id])
+    if @selected_cat
+      @category_list[@selected_cat] += ' sidebar-selected'
+
+      # TODO: filter only files with moved_to=-1
+      @files = TranslationFile.find(:all, :conditions => ['filename_with_path LIKE ?', @selected_cat + '/%'])
+    end
   end
 
   verify :method => :post, :xhr => true, :only => :ajax_lock_pofile, #[:ajax_lock_pofile, :ajax_upload],
