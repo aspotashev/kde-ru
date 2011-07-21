f4da9f5c508da6c39337e60eb7bac6c51217c841
Alexander Potashev <aspotashev@gmail.com>
2011-07-21 04:19:46 +0400

    JS: use plain onclick="" instead of live()

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,14 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+function ajax_lock_pofile(id)
+{
+  $.post('/translation_file/ajax_lock_pofile/' + id,
+         function(data) { $("tr.ajax_lock_div#a" + id).html(data); },
+         {}, {}, 'html');
+}
+
 $(document).ready(function(){
-
-//      $("table").delegate("a", "click", function(){
-  $("a.ajax_lock").live("click", function(){ // people say that "live" fails on some browsers
-    clicked_link_id = $(this).attr('id');
-    $.post('/translation_file/ajax_lock_pofile/' + clicked_link_id.substr(1), function(data){
-      $("tr.ajax_lock_div#" + clicked_link_id).html(data);
-    }, {}, {}, 'html');
-  });
-
+    // .....
 });
